e24ad2f31ca8f386c85d4384bb1056d9412ceb42
Yash Jaiswal <yashjaiswal2121@gmail.com>
2023-02-03 22:46:10 +0530

    second

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -5,7 +5,7 @@ const fetchuser=(req,res,next)=>{
     //Get the user from the jwt token and add id to req object
     const token=req.header('auth-token');   
     if(!token){
-        res.status(401).send({error:"Please authenticate using a valid token"})
+        return res.status(401).send({error:"Please authenticate using a valid token"})
     }
     try{
         const data =jwt.verify(token,JWT_SECRET)    //verify the auth-token (which is generated as a response of login correctly)
@@ -14,7 +14,7 @@ const fetchuser=(req,res,next)=>{
         next(); //called async func in 3rd argument
     }
     catch(error){
-        res.status(401).send({error:"Please authenticate using a valid token"})
+        return res.status(401).send({error:"Please authenticate using a valid token"})
     }
 }
 
